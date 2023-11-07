2817ccb715d205d1449f3bafa6470b477ba3c8e8
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-11-07 18:38:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ('SESSION', 'Media_search')
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
