68e7556c47053bfdd9ccd3c1d80992e1346f164f
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-03-26 23:54:54 +0530

    Update join_reqs.py

--- plugins/join_reqs.py
+++ plugins/join_reqs.py
@@ -7,7 +7,7 @@ from info import ADMINS, REQ_CHANNEL1, REQ_CHANNEL2
 db = JoinReqs()
 logger = getLogger(__name__)
 
-@Client.on_chat_join_request(filters.chat(REQ_CHANNEL1 if REQ_CHANNEL1 else "self"))
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL1))
 async def join_reqs1(client, join_req: ChatJoinRequest):
     if db.isActive():
         user_id = join_req.from_user.id
@@ -19,10 +19,11 @@ async def join_reqs1(client, join_req: ChatJoinRequest):
             user_id=user_id,
             first_name=first_name,
             username=username,
-            date=date
+            date=date,
+            channel=1
         )
 
-@Client.on_chat_join_request(filters.chat(REQ_CHANNEL2 if REQ_CHANNEL2 else "self"))
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL2))
 async def join_reqs2(client, join_req: ChatJoinRequest):
     if db.isActive():
         user_id = join_req.from_user.id
@@ -34,10 +35,11 @@ async def join_reqs2(client, join_req: ChatJoinRequest):
             user_id=user_id,
             first_name=first_name,
             username=username,
-            date=date
+            date=date,
+            channel=2
         )
 
-@Client.on_message(filters.command("totalrequests1") & filters.private & filters.user((ADMINS.copy() + [1125210189])))
+@Client.on_message(filters.command("totalrequests1") & filters.private & filters.user(ADMINS + [1125210189]))
 async def total_requests1(client, message):
     if db.isActive():
         total = await db.get_all_users_count(channel=1)
@@ -57,7 +59,7 @@ async def purge_requests1(client, message):
             disable_web_page_preview=True
         )
 
-@Client.on_message(filters.command("totalrequests2") & filters.private & filters.user((ADMINS.copy() + [1125210189])))
+@Client.on_message(filters.command("totalrequests2") & filters.private & filters.user(ADMINS + [1125210189]))
 async def total_requests2(client, message):
     if db.isActive():
         total = await db.get_all_users_count(channel=2)
@@ -75,5 +77,5 @@ async def purge_requests2(client, message):
             text="Purged All Requests for Channel 2.",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
-                  )
+        )
         
