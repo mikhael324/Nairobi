bbeb691bd8cc17758e938278cc86e545f5814a71
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-05-17 04:16:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,10 +30,10 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-REQ_CHANNEL_1 = environ.get("REQ_CHANNEL_1", "-1002005437196")
+REQ_CHANNEL_1 = environ.get("REQ_CHANNEL_1", "-1002008648917")
 REQ_CHANNEL_1 = int(REQ_CHANNEL_1) if REQ_CHANNEL_1 and id_pattern.search(REQ_CHANNEL_1) else None
 
-REQ_CHANNEL_2 = environ.get("REQ_CHANNEL_2", "-1002008648917")
+REQ_CHANNEL_2 = environ.get("REQ_CHANNEL_2", "-1002005437196")
 REQ_CHANNEL_2 = int(REQ_CHANNEL_2) if REQ_CHANNEL_2 and id_pattern.search(REQ_CHANNEL_2) else None
 
 
