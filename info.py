c554eed2ef3662c645a2ebec8d1a4e75c2663f1f
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-06-23 13:46:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-SPELL_LNK = environ.get('SPELL_LNK', 'https://t.me/+3xV5gXG5wfthZTU1')
+SPELL_LNK = environ.get('SPELL_LNK', 'https://t.me/+s8k67Q_e9FY4Yzk1')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
