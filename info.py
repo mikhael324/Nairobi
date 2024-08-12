5652c085208e0039490194f0763c7778ee0fc751
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-08-12 12:01:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '22085172'))
-API_HASH = environ.get('API_HASH', '9b8613cc83ab0c66d12385664f09c6de')
+API_ID = int(environ.get('API_ID', '5443727'))
+API_HASH = environ.get('API_HASH', '705b16e33a114de9491252d0a448921c')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6847306010:AAG6LUBCsSDJAjtQZG4PWOOwN2u8f0XJroI')
 
 # Bot settings
@@ -22,7 +22,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://graph.org/file/8fb97aac351471c4c91b9.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '823191583 1746132193').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1746132193 509051317 997743002').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001761183308 -1001702551529 -1001227981388 -1001738644907 -1001794047221').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
